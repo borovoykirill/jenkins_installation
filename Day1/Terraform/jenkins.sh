00fe3c652c41dd133949f64f58b69287ffa79f1c
slabuke <dedbabay91@mail.ru>
2020-04-03 00:32:19 +0300

    1-st day

---
+++ Day1/Terraform/jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+until [[ "$rez" = "0%" ]]
+do
+rez=$(ping -c 1 8.8.8.8 | grep % | cut -d ' ' -f6)
+echo "waiting for internet"
+done
+echo "Internet connection is stable"
+
+yum update -y
+yum install epel-release -y
+yum install git -y
+yum install ansible -y
+# git clone https://github.com/slabuke/Jenkins_D1
+# ansible-playbook ./Jenkins_D1/ansible/jenkins.yml
+
+echo "ALL DONE"
