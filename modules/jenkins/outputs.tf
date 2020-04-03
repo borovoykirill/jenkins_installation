18fc5f752c8907a3b0f35a2d5bd6377bc6d6574b
Borovoy Kirill <borovoy.kirill@yandex.by>
2020-04-03 10:31:51 +0300

    final v.2

--- modules/jenkins/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "jenkins" {
-    value = "${google_compute_instance.ubuntu-jenkins.network_interface.0.access_config.0.nat_ip}"
-               
-}
--- variable.tf
+++
@@ -1,8 +0,0 @@
-variable "project" {
-  default = "focus-task-272708"
-}
-
-variable "region" {
-  default  = "us-central1"
-}
-  
