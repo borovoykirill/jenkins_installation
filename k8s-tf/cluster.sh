b4c6ff0bcbeceb79f25095078ae6d3d2c1b87956
Vitalyazavr <vitalis03@mail.ru>
2020-04-04 13:20:21 +0300

    With completed ingress

---
+++ k8s-tf/cluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+#to deploy configurtion in kubernetes cluster
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+kubectl apply -f jenkins.yml -n jenkins
